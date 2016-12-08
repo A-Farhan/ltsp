b099844135b203703668a955f206bf47dedbf7d1
A-Farhan <ali3929@gmail.com>
2016-12-08 22:38:51 +0530

    Update entropy_func.R

--- entropy_func.R
+++ entropy_func.R
@@ -1,6 +1,6 @@
 # function to calculate shannon's entropy
 entH <- function(x){
-  # removing '0' frequncies as is considered as 0
+  # removing '0' frequencies as is considered as 0
   x = x[ x > 0 ]
   # calculating shannons entropy for the rest & rounding off to 3 digits
   y = - sum( x * log2(x) ) 
